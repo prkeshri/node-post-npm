651e9516966c7cb3ad515f8395a945926a306b2d
Praveen <prkeshri@gmail.com>
2021-10-17 22:51:08 +0530

    Fixes

--- README.md
+++ README.md
@@ -24,7 +24,8 @@ npm install post-npm
 
 ## Use:
 1. Add in dependencies
-2. In postinstall script, add: post-npm &lt;script to execute&gt; &lt;script arguments&gt;
+2. In postinstall script, add: post-npm &lt;script to execute&gt; \[-once\] &lt;script arguments&gt;
+3. In case of -once, multiple calls with same scripts will be executed only once!
 ## Author
 
 👤 **Praveen Ranjan Keshri**
--- post-npm.js
+++ post-npm.js
@@ -1,8 +1,24 @@
 #!/usr/bin/env node
 const { spawn, exec } = require('child_process');
 const [node, me, npmid, cmd, ...args] = process.argv;
+const os = require('os');
+const fs = require('fs');
+const uniqueHash = require("unique-hash");
 let i;
 let finished = false;
+const flagargs = [];
+while(args[0] && args[0][0] === '-') {
+    flagargs.push(args.shift());
+}
+
+let tmp;
+if(flagargs[0] === '-once') {
+    tmp = path.join(os.tmpdir(), uniqueHash(args.join(' ')));
+    if(fs.existsSync(tmp)) {
+        return;
+    }
+    fs.writeFileSync(tmp, "");
+}
 p = _ => {
     let k = exec(`kill -0 ${npmid}`);
     k.on('close', code => {
@@ -16,6 +32,9 @@ p = _ => {
                 spawn(cmd, args, {
                     stdio: [ 'ignore', process.stdout, 'ignore' ], 
                 });
+                if(tmp) {
+                    fs.unlinkSync(tmp);
+                }
             } catch (e) {
             }
         }
